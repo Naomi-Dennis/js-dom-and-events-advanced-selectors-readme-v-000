f417b863c42a4aca9aeb1ae6138329cadf404602
Naomi Dennis <njdennis94@gmail.com>
2018-10-22 16:02:32 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,21 @@
-// declare your functions here...
+// it('paragraphSelector() should select all paragraphs', function() {
+//     expect(paragraphSelector().length).toBe(1);
+//     expect(paragraphSelector()[0].innerHTML).toEqual($('p')[0].innerHTML);
+//   });
+$(document).ready( 
+  init
+  )
+function init(){
+}
+function paragraphSelector(){
+  return $("p") 
+}
+
+// it('lastImageSelector() should return the last image', function() {
+//     expect(lastImageSelector().length).toBe(1);
+//     expect(lastImageSelector().attr('src')).toEqual("https://s3.amazonaws.com/after-school-assets/confused.gif");
+//   });
+
+function lastImageSelector(){
+  return $("")
+}
